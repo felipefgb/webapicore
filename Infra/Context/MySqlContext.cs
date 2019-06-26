5b3f7a31e69432f9d3825a4fec50348647080556
Felipe Guilherme Bastos <FBASTOS@firjan.com.br>
2019-06-26 12:29:05 -0300

    Versionando o codigo

---
+++ Infra/Context/MySqlContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using webapicore.Domain.Entities;
+using webapicore.Infra.Data.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webapicore.Infra.Data.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(){}
+        public MySqlContext(DbContextOptions<MySqlContext> options)
+            : base(options){}
+
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                //optionsBuilder.UseSqlServer(@"Server=SES17110\SQLEXPRESS;Database=Db;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\ProjetoDotNet\modelo-api-master\Infra\AppData\Database1.mdf;Integrated Security=True");
+
+            //<add name="CustodiaAtivosContext" connectionString="data source=srvsede0157;initial catalog=CustomizacoesHD;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework" providerName="System.Data.SqlClient" />
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+        }
+    }
+}
